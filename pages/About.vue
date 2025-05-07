870986af4d18662e0dad01a5a5030a80b237afbc
oriyuta39 <oriyuta061139@gmail.com>
2025-05-07 13:40:59 +0900

    update 05/07

--- pages/About.vue
+++ pages/About.vue
@@ -3,6 +3,7 @@
 		<section>
 			<h1>About</h1>
 			<section>
+				<h2>基本情報</h2>
 				<table>
 					<tbody>
 						<tr>
@@ -17,10 +18,22 @@
 							<th>Role:</th>
 							<td>Technical school student</td>
 						</tr>
+						<tr>
+							<th></th>
+							<td></td>
+						</tr>
 					</tbody>
 				</table>
 			</section>
-			<p>aaa <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></p>
+			<section>
+				<h2>自己紹介</h2>
+				<div>
+					<p>現在、情報系専門学校に在学し、IT分野の技術を幅広く学んでいます。</p>
+					<p>中学卒業以降、しばらくの間、自分の進路に迷う時期が続いていましたが、
+						「新しいことに挑戦したい」という思いから、ITの世界に興味を持ち、学び始めました。</p>
+				</div>
+			</section>
+			<p>aaa</p>
 		</section>
 	</div>
 </template>
