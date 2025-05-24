2176ab1b92bc5363bb81a3815d58d7ee8ee74a3e
oriyuta39 <oriyuta061139@gmail.com>
2025-05-24 17:31:30 +0900

    update 05/24

--- pages/Contact.vue
+++ pages/Contact.vue
@@ -30,7 +30,7 @@
       <section class="container-contact">
         <h2 class="m-4">お問い合わせ</h2>
         <p>準備中</p>
-        <form name="contact" method="POST" netlify>
+        <form name="contact" method="POST">
           <input type="hidden" name="form-name" value="contact">
           <div class="row">
             <div class="col-md-6 mb-3">
@@ -45,9 +45,11 @@
           </div>
           <div class="mb-3">
             <label for="message" class="form-label">お問い合わせ内容</label>
-            <textarea id="message" name="message" class="form-control" rows="10" placeholder="お問い合わせ内容を入力" required />
+            <textarea id="message" name="message" type="text" class="form-control" rows="10" placeholder="お問い合わせ内容を入力"
+              required />
           </div>
           <input id="submit" type="submit" value="送信" class="btn btn-primary px-3">
+          <input type="hidden" name="bot-field">
         </form>
       </section>
     </div>
